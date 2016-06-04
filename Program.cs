58b9d9b4a18a8285902900601bfd74ced7bbebfb
BuLLeTCode <raivis.rugelis@va.lv>
2016-06-04 22:33:06 +0300

    Project + updated readme.md

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    internal static class DayTimeUtilities
+    {
+        public static int DayToEndMonth(this DateTime date)//Norādot parametrā vārdu this, šī kļūst par Extension metodi, nav nepieciešams izmantot
+        {                                                  //DayTimeUtilities keyword lai piekļūtu metodei, var izmantot date.DayToEndMonth piemēram.
+            return DateTime.DaysInMonth(date.Year, date.Month) - date.Day;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            DateTime randomDate = new DateTime(2010, 8, 29);
+
+            int dayTillEndOfMonth = randomDate.DayToEndMonth();
+
+            Console.WriteLine("Days till month end: " + dayTillEndOfMonth + " for: " + randomDate.ToShortDateString());
+            Console.ReadKey();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Extension-method-example
-C# Extension method example
+# Extension method example
+C# Extension method example. Extension methods are cool C# feature, where you can "add" methods to existing types without creating a new derived type. In Parameters using "this" keyword.
